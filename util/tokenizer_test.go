ca8923cb05903a9ebeacafa8b0be14ae6d2a54c3
wangy <wangy@transip.nl>
2022-01-03 22:24:05 +0100

    reformat code

--- util/tokenizer_test.go
+++ util/tokenizer_test.go
@@ -6,41 +6,41 @@ import (
 )
 
 func Test_Tokenizer(t *testing.T) {
-	text1 := "我是一个中国人"
-	result1, err := Tokenizer(text1)
+	text := "我是一个中国人"
+	result, err := Tokenizer(text)
 	if err != nil {
-		t.Errorf("Error in tokenizing text: %s", text1)
+		t.Errorf("Error in tokenizing text: %s", text)
 	}
-	assert.Equal(t, []string{"我", "是", "一个", "中国", "人"}, result1)
+	assert.Equal(t, []string{"我", "是", "一个", "中国", "人"}, result)
 
-	text2 := "明天天气会更好"
-	result2, err := Tokenizer(text2)
+	text = "明天天气会更好"
+	result, err = Tokenizer(text)
 	if err != nil {
-		t.Errorf("Error in tokenizing text: %s", text2)
+		t.Errorf("Error in tokenizing text: %s", text)
 	}
-	assert.Equal(t, []string{"明天", "天气", "会", "更好"}, result2)
+	assert.Equal(t, []string{"明天", "天气", "会", "更好"}, result)
 
 	// special symbols in the sentence can be tokenized correctly
-	text3 := "你们说：怎么安排？"
-	result3, err := Tokenizer(text3)
+	text = "你们说：怎么安排？"
+	result, err = Tokenizer(text)
 	if err != nil {
-		t.Errorf("Error in tokenizing text: %s", text3)
+		t.Errorf("Error in tokenizing text: %s", text)
 	}
-	assert.Equal(t, []string{"你们", "说", "：", "怎么", "安排", "？"}, result3)
+	assert.Equal(t, []string{"你们", "说", "：", "怎么", "安排", "？"}, result)
 
 	// English word single appearance in Chinese sentence can be tokenized.
-	text4 := "我们明天一起camping你觉得怎么样？"
-	result4, err := Tokenizer(text4)
+	text = "我们明天一起camping你觉得怎么样？"
+	result, err = Tokenizer(text)
 	if err != nil {
-		t.Errorf("Error in tokenizing text: %s", text4)
+		t.Errorf("Error in tokenizing text: %s", text)
 	}
-	assert.Equal(t, []string{"我们", "明天", "一起", "camping", "你", "觉得", "怎么样", "？"}, result4)
+	assert.Equal(t, []string{"我们", "明天", "一起", "camping", "你", "觉得", "怎么样", "？"}, result)
 
 	// English words multiple appearances in Chinese sentence can be tokenized.
-	text5 := "他说：'How are you?'"
-	result5, err := Tokenizer(text5)
+	text = "他说：'How are you?'"
+	result, err = Tokenizer(text)
 	if err != nil {
-		t.Errorf("Error in tokenizing text: %s", text5)
+		t.Errorf("Error in tokenizing text: %s", text)
 	}
-	assert.Equal(t, []string{"他", "说", "：", "'", "How", " ", "are", " ", "you", "?", "'"}, result5)
+	assert.Equal(t, []string{"他", "说", "：", "'", "How", " ", "are", " ", "you", "?", "'"}, result)
 }
