9078919c3f7412b3e53e53f9c669fdff48756960
wangy <wangy@transip.nl>
2022-12-06 17:29:54 +0100

    heroku setting

--- main.go
+++ main.go
@@ -12,6 +12,9 @@ func main() {
 		fmt.Fprintf(w, "Hello, World!")
 	})
 	fmt.Printf("Server running (port=8080), route: http://localhost:8080/helloworld\n")
+
+	// Read this on heroku dynamic port number
+	// https://stackoverflow.com/questions/56936448/deploying-a-golang-app-on-heroku-build-succeed-but-application-error
 	port := os.Getenv("PORT")
 	if err := http.ListenAndServe(port, nil); err != nil {
 		log.Fatal(err)
