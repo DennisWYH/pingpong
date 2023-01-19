cc44964b7a292f1c91262e0f55aba73e68a22125
wangy <wangy@transip.nl>
2023-01-19 17:38:30 +0100

    testing

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 		fmt.Fprintf(w, "Hello, World!")
 	})
 	http.HandleFunc("/helloworld", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, World!")
+		w.Write([]byte("hello.world"))
 	})
 
 	// Read this on heroku dynamic port number
