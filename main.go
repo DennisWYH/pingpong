0f29b1af0eb8a3033d726e1a911cb04f6bb03f7c
wangy <wangy@transip.nl>
2023-01-19 17:30:36 +0100

    remove favicon handler

--- main.go
+++ main.go
@@ -11,13 +11,9 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, World!")
 	})
-	http.HandleFunc("/favicon.ico", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, World!")
-	})
 	http.HandleFunc("/helloworld", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, World!")
 	})
-	fmt.Printf("Server running (port=8080), route: http://localhost:8080/helloworld\n")
 
 	// Read this on heroku dynamic port number
 	// https://stackoverflow.com/questions/56936448/deploying-a-golang-app-on-heroku-build-succeed-but-application-error
