c74ea2ecbc1a10aae41eb40103fd94dc1d28e3e6
wangy <wangy@transip.nl>
2023-01-19 18:26:35 +0100

    ad : to the port

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ func main() {
 	// Read this on heroku dynamic port number
 	// https://stackoverflow.com/questions/56936448/deploying-a-golang-app-on-heroku-build-succeed-but-application-error
 	port := os.Getenv("PORT")
-	if err := http.ListenAndServe(port, nil); err != nil {
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
 		log.Fatal(err)
 	}
 }
