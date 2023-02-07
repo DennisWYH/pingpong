b489dd5d46bd635d6fa9f8ea58824727797cb069
wangy <wangy@transip.nl>
2023-02-07 15:41:59 +0100

    update change

--- main.go
+++ main.go
@@ -8,12 +8,28 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	// APIs for frontend actions
+	http.HandleFunc("/next", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, World!")
+	})
+	http.HandleFunc("/previous", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, World!")
 	})
 	http.HandleFunc("/helloworld", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("hello.world"))
 	})
+
+	// APIs for database CRUD management
+	http.HandleFunc("/add-sentence", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("this page displays a form where user can add sentences"))
+	})
+
+	//http.HandleFunc("/remove-sentence", func(w http.ResponseWriter, r *http.Request) {
+	//	w.Write([]byte("hello.world"))
+	//})
+	//http.HandleFunc("/list-sentence", func(w http.ResponseWriter, r *http.Request) {
+	//	w.Write([]byte("hello.world"))
+	//})
 
 	// Read this on heroku dynamic port number
 	// https://stackoverflow.com/questions/56936448/deploying-a-golang-app-on-heroku-build-succeed-but-application-error
